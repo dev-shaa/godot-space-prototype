a96fc52adc15e3489e685e2c92d3682976514124
Salvatore Di Gennaro <salvatore.digennaro@outlook.it>
2025-06-13 19:01:20 +0200

    Add moving functionality

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -1,20 +1,78 @@
-extends Node
+extends RigidBody3D
 
-@export var rigidbody: RigidBody3D
+@export var camera: Node3D
+@export_range(0, 1) var sensitivity: float = 0.5
+@export var roll_speed: float = 1
+@export var push_raycast: RayCast3D
+@export var push_force: float = 1
+@export var joint: Joint3D
 
-var yaw: float
+var pitch: float = 0
+var roll: float = 0
+var yaw: float = 0
+var should_push: bool = false
 
-# Called when the node enters the scene tree for the first time.
+var close_handle: Node3D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	pass
 
 func _physics_process(delta):
-	# rigidbody.rotate_x(roll * delta)
-	rigidbody.rotate_y(yaw * delta)
-	# rigidbody.rotate_z(pitch * delta)
+	var forward := camera.global_basis * Vector3.FORWARD
+	var right := camera.global_basis * Vector3.RIGHT
+	var up := camera.global_basis * Vector3.UP
+
+	roll = Input.get_axis("roll_left", "roll_right")
+
+	camera.rotate(forward, roll * roll_speed * delta)
+	camera.rotate(right, -pitch * sensitivity * delta)
+	camera.rotate(up, yaw * sensitivity * delta)
+
+	if should_push:
+		if can_push():
+			apply_central_impulse(forward * push_force)
+
+		should_push = false
 
 	yaw = 0
+	roll = 0
+	pitch = 0
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		yaw += event.relative.x
+		yaw -= event.relative.x
+		pitch += event.relative.y
+		pass
+	elif event.is_action_pressed("push"):
+		should_push = true
+	elif event.is_action_pressed("grab"):
+		grab(close_handle)
+	elif event.is_action_released("grab"):
+		grab(null)
+	elif event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+func _on_area_3d_body_entered(body: Node3D):
+	if close_handle == null:
+		close_handle = body
+	pass
+
+func _on_area_3d_body_exited(body: Node3D):
+	if body == close_handle:
+		close_handle = body
+	pass
+
+func can_push() -> bool:
+	return true
+	return push_raycast.is_colliding()
+
+func grab(handle: Node3D) -> void:
+	if handle:
+		joint.node_a = self.get_path()
+		joint.node_b = handle.get_path()
+		print("Grabbed handle")
+	else:
+		joint.node_a = NodePath()
+		joint.node_b = NodePath()
+		print("Released handle")
